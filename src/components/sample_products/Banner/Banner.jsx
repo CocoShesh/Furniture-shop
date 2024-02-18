d115edd4d431c3822a3af38d9d06ad8f1182e3c5
NARD <110368170+CocoShesh@users.noreply.github.com>
2024-02-18 23:16:25 +0800

    Update Banner.jsx

--- src/components/sample_products/Banner/Banner.jsx
+++ src/components/sample_products/Banner/Banner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import banner from "../../../assets/banner.jpg";
 function Banner() {
   return (
-    <div className="flex border-2 mx-20 mb-10 font-san font h-[500px] max-sm:w-full max-sm:flex-col-reverse max-sm:mx-auto max-sm:h-full max-lg:w-autp max-lg:mx-5 ">
+    <div className="flex border-2 mx-10 mb-10 font-san font h-[500px] max-sm:w-full max-sm:flex-col-reverse max-sm:mx-auto max-sm:h-full max-lg:w-auto max-lg:mx-5   ">
       <section className="  bg-[#e9e9e9] pl-32 pr-10 pt-44 w-[600px] max-sm:w-auto max-sm:p-10  max-lg:w-auto max-lg:px-5">
         <h1 className=" font-semibold text-3xl mb-5 text-gray-800">
           Creative harmonious living
@@ -17,11 +17,11 @@ function Banner() {
           </button>
         </a>
       </section>
-      <section className="max-lg:w-full">
+      <section className="max-lg:w-full 2xl:max-w-auto">
         <img
           src={banner}
           alt={banner}
-          className=" w-[650px] h-full max-lg:w-full  max-lg:object-cover"
+          className=" w-[675px] h-full max-lg:w-full  2xl:w-[2050px]  2xl:object-cover  max-lg:object-fill "
         />
       </section>
     </div>
